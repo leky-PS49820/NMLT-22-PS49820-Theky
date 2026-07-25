44828424d8e7fb1236c9c10fbe631e19de4da65d
leky-PS49820 <leky9850@gmail.com>
2026-07-25 11:18:05 +0700

    Lab4: Hoan thanh Chuc nang 1

--- Main_Menu.c
+++ Main_Menu.c
@@ -18,9 +18,40 @@ int main() {
         scanf("%d", &chon);
 
         switch (chon) {
-            case 1:
-                printf("\n--- Chuc nang 1 dang duoc phat trien ---\n\n");
+           case 1: {
+                int min, max;
+                int tong = 0;
+                int bienDem = 0;
+
+                printf("\n--- CHUC NANG 1: TINH TRUNG BINH TONG CAC SO CHIA HET CHO 2 ---\n");
+                printf("Nhap vao gia tri min: ");
+                scanf("%d", &min);
+                printf("Nhap vao gia tri max: ");
+                scanf("%d", &max);
+
+                // Xuly truong hop nguoi dung nhap min > max
+                if (min > max) {
+                    printf("Loi: Gia tri min phai nho hon hoac bang max!\n\n");
+                    break;
+                }
+
+                // Duyet cac so tu min den max bang vong lap for
+                for (int i = min; i <= max; i++) {
+                    if (i % 2 == 0) {
+                        tong += i;
+                        bienDem++;
+                    }
+                }
+
+                // Bat loi chia cho 0 (division by zero)
+                if (bienDem == 0) {
+                    printf("Khong co so nao chia het cho 2 trong khoang [%d, %d].\n\n", min, max);
+                } else {
+                    float trungBinh = (float)tong / bienDem;
+                    printf("Trung binh tong cac so chia het cho 2 tu %d den %d la: %.2f\n\n", min, max, trungBinh);
+                }
                 break;
+            }
             case 2:
                 printf("\n--- Chuc nang 2 dang duoc phat trien ---\n\n");
                 break;
